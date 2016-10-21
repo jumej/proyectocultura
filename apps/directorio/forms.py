f84a0e515c5626eb9960413d262939538cb7f088
Alex Gomez <alex.gomez.1814@gmail.com>
2016-10-21 15:56:27 -0600

    plantilla experimental

--- apps/directorio/forms.py
+++ apps/directorio/forms.py
@@ -1,6 +1,7 @@
 #Archivo que llevara todos los formularios
 from django import forms
 from apps.directorio.models import *
+
 class ArtistaForm(forms.ModelForm):
 	class Meta:
 		model = Artista
@@ -30,15 +31,10 @@ class ArtistaForm(forms.ModelForm):
 
 		widgets = {
 
-			'nombre': forms.TextInput(attrs={'class':'form-control'}),
+			'nombre': forms.TextInput(attrs={'title': 'Tu nombre','class':'form-control'}),
 			'apellido': forms.TextInput(attrs={'class':'form-control'}),
 			'genero': forms.RadioSelect,
-			'fecha_nacimiento': forms.TextInput(attrs={'class':'form-control'}),
 			'telefono': forms.TextInput(attrs={'class':'form-control'}),
 			'email': forms.TextInput(attrs={'class':'form-control'}),
 			'categoria': forms.Select(attrs={'class':'form-control'}),
-			'descripcion': forms.TextInput(attrs={'class':'form-control'}),
-
-
-
 		}
--- proyecto/settings.py
+++ proyecto/settings.py
@@ -85,5 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,'templates'),)
+STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'static'),)
